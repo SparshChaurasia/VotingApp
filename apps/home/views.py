7f60719d5fd55f3271312c16220676f00edd2cfb
SparshChaurasia <sparshchaurasia0021@gmail.com>
2023-04-09 12:57:04 +0530

    improved current events section

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-from apps.vote.models import Event
+from apps.vote.models import Event, Student, Option, Category
+
 
 
 @login_required(login_url="/login")
@@ -34,13 +35,22 @@ def dashboard(request):
     #     s_voted = students.filter(Voted=True).count()
     #     s_not_voted = students.filter(Voted=False).count()
 
-    events = Event.objects.all()
-    params = {
-        "events": events,
+    # events = Event.objects.all()
+    # params = {
+    #     "events": events,
         # "class": s_class, 
         # "total": s_total,
         # "voted": s_voted, 
         # "not_voted": s_not_voted
-    }
-
-    return render(request, "dashboard.html", params)
+    # }
+    params = {}
+    # students = Student.objects.all()
+    
+    events = Event.objects.all()
+    for event in events:
+        params[event] = {}
+        categorys = Category.objects.filter(Event=event)
+        for category in categorys:
+            options = Option.objects.filter(OpitonCategory=category)
+            params[event].update({category: options})
+    return render(request, "dashboard.html", {"params":params})
