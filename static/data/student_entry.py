a6fc747a40fabd144ef999443d440c168b1e4038
SparshChaurasia <sparshchaurasia0021@gmail.com>
2023-04-24 23:01:29 +0530

    added student accounts

---
+++ static/data/student_entry.py
@@ -0,0 +1,10 @@
+import csv
+
+from apps.vote.models import Student
+
+with open("./StudentsList.csv", mode="r") as f:
+    csv_file = csv.reader(f)
+
+    for line in csv_file:
+        student = Student.objects.create(StudentID=line[0], Class=line[1], Name=line[2])
+        print(line[0], line[1], line[2],"...Done")
